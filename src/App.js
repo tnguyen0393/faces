7699fb1564f7dad0ffd2f66c7cfe03ff805a3a89
Tien Nguyen <tnguyen0393@gmail.com>
2020-06-30 15:27:47 -0500

    face detecting model api added. facerecognition image styled to specific width.

--- src/App.js
+++ src/App.js
@@ -29,23 +29,23 @@ class App extends Component {
     super();
     this.state = {
       input: "",
+      imageUrl: "",
     };
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({ input: event.target.value });
   };
 
   onButtonSubmit = () => {
-    console.log("clicked!!!!!");
+    this.setState({ imageUrl: this.state.input });
     app.models
-      .predict(
-        "a403429f2ddf4b49b307e318f00e528b",
-        "https://samples.clarifai.com/face-det.jpg"
-      )
+      .predict("a403429f2ddf4b49b307e318f00e528b", this.state.input)
       .then(
         function (response) {
-          console.log(response);
+          console.log(
+            response.outputs[0].data.regions[0].region_info.bounding_box
+          );
         },
         function (err) {
           // there was an error
@@ -64,7 +64,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
