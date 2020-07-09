69f75e1b68d3a07a78bf35f860ecf2110977635f
Tien Nguyen <tnguyen0393@gmail.com>
2020-07-08 19:40:53 -0500

    fixed refresh bug when signing in

--- src/components/facerecognition/facerecognition.component.jsx
+++ src/components/facerecognition/facerecognition.component.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import "./facerecognition.style.css";
+import React from 'react';
+import './facerecognition.style.css';
 
 const FaceRecognition = ({ imageUrl, box }) => {
   return (
-    <div className="center ma">
-      <div className="absolute mt2">
+    <div className='center ma'>
+      <div className='absolute mt2'>
         <img
-          id="inputimage"
+          id='inputimage'
           src={imageUrl}
-          alt="imgHere"
-          width="500px"
-          height="auto"
+          alt=''
+          width='500px'
+          height='auto'
         />
         <div
-          className="bounding-box"
+          className='bounding-box'
           style={{
             top: box.topRow,
             right: box.rightCol,
