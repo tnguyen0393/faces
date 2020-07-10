79e10d321021eac0ce029f55f286345b4e43eed5
Tien Nguyen <tnguyen0393@gmail.com>
2020-07-10 00:15:10 -0500

    changed end points for api

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -17,7 +17,7 @@ class SignIn extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://stark-sands-78704.herokuapp.com/signin', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
